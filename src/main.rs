80862f9975f97801bd1e9c46dcf4c96d09e84139
Lalo Martins <lalo.martins@gmail.com>
2020-04-21 17:13:57 +0300

    dump storage on exit

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ use tokio::signal;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let storage = MemoryStorage::default();
-    let server = event_server::grpc::Server::<MemoryStorage> {storage: Arc::new(storage)};
+    let storage = Arc::new(MemoryStorage::default());
+    let server = event_server::grpc::Server::<MemoryStorage> {storage: Arc::clone(&storage)};
 
     Server::builder()
         .add_service(event_server::grpc::event_server_server::EventServerServer::new(server))
         .serve_with_shutdown(addr, async {signal::ctrl_c().await.expect("failed to listen to interrupt signal");})
         .await?;
 
-    println!("Interrupted.");
+    println!("Interrupted. Dumping storage for debugging:\n{:?}", *storage);
     Ok(())
 }
