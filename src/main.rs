f8cdf45e826d50032943314dd5f6a297de5ddcd0
Lalo Martins <lalo.martins@gmail.com>
2020-04-21 15:55:37 +0300

    stop server on ctrl-c

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use tonic::transport::Server;
 mod event_server;
 use event_server::memory_storage::MemoryStorage;
+use tokio::signal;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,8 +12,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Server::builder()
         .add_service(event_server::grpc::event_server_server::EventServerServer::new(server))
-        .serve(addr)
+        .serve_with_shutdown(addr, async {signal::ctrl_c().await.expect("failed to listen to interrupt signal");})
         .await?;
 
+    println!("Interrupted.");
     Ok(())
 }
