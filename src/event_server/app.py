91f3fe6288555cc341fbabd2d57821a26baca10f
Lalo Martins <lalo.martins@gmail.com>
2024-01-08 00:00:29 +0900

    id argument

--- src/event_server/app.py
+++ src/event_server/app.py
@@ -1,6 +1,7 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import UUID1
 
 from .model.event import Event, sample
 
@@ -12,6 +13,8 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return sample
+@app.get("/events/{item_id}")
+def read_item(item_id: UUID1):
+    if item_id == sample.uuid:
+        return sample
+    raise HTTPException(status_code=404, detail="Item not found")
