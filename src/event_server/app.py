41b60c3a15ac63b377bf7df84404dbaaa335372e
Lalo Martins <lalo.martins@gmail.com>
2024-01-08 00:06:58 +0900

    namespace with application and account

--- src/event_server/app.py
+++ src/event_server/app.py
@@ -1,6 +1,6 @@
-from typing import Union
+from typing import Annotated, Union
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Header
 from pydantic import UUID1
 
 from .model.event import Event, sample
@@ -14,7 +14,15 @@ def read_root():
 
 
 @app.get("/events/{item_id}")
-def read_item(item_id: UUID1):
-    if item_id == sample.uuid:
+def read_item(
+    item_id: UUID1,
+    application: Annotated[str | None, Header(alias="x-application")] = None,
+    account: Annotated[str | None, Header(alias="x-account")] = None,
+):
+    if (
+        item_id == sample.uuid
+        and application == sample.application
+        and account == sample.account
+    ):
         return sample
     raise HTTPException(status_code=404, detail="Item not found")
